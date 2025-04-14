7340a14ecc4d0ad21789c89053b28fb7af6a765d
Fernando <nandodinan111@gmail.com>
2025-04-14 13:51:41 +0700

    app.py

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
